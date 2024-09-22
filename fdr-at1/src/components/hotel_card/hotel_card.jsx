2682bfd735b4b50c4f5be5147ede21e629ef39d0
ItzTas <talessabini2@gmail.com>
2024-09-22 00:26:50 -0400

    chore: hotel card mudado

--- fdr-at1/src/components/hotel_card/hotel_card.jsx
+++ fdr-at1/src/components/hotel_card/hotel_card.jsx
@@ -3,44 +3,47 @@ import { useState } from 'react';
 import styles from './hotel_card.module.css';
 
 export default function HotelCard({ hotelSiteURL, imgURL, name, desc, stars }) {
-    const [imgLoaded, setImgLoaded] = useState(true);
+  const [imgLoaded, setImgLoaded] = useState(true);
+  if (desc.length > 63) {
+    desc = desc.substring(0, 60) + '...';
+  }
 
-    function renderStars() {
-        const starElements = [];
-        for (let i = 0; i < stars; i++) {
-            starElements.push(
-                <span key={i} className={styles.hotelStar}>
-                    ⭐
-                </span>,
-            );
-        }
-        return starElements;
+  function renderStars() {
+    const starElements = [];
+    for (let i = 0; i < stars; i++) {
+      starElements.push(
+        <span key={i} className={styles.hotelStar}>
+          ⭐
+        </span>,
+      );
     }
-    return (
-        <a href={hotelSiteURL}>
-            <div className={styles.hotelCard}>
-                {imgLoaded ? (
-                    <img
-                        className={styles.imgCard}
-                        src={imgURL}
-                        alt={name}
-                        onError={() => setImgLoaded(false)}
-                    />
-                ) : (
-                    <div className={styles.imgCard}>Imagem não disponivel</div>
-                )}
-                <h2 className={styles.hotelName}>{name}</h2>
-                <p>{desc}</p>
-                <div className={styles.hotelStars}> {renderStars()}</div>
-            </div>
-        </a>
-    );
+    return starElements;
+  }
+  return (
+    <a href={hotelSiteURL}>
+      <div className={styles.hotelCard}>
+        {imgLoaded ? (
+          <img
+            className={styles.imgCard}
+            src={imgURL}
+            alt={name}
+            onError={() => setImgLoaded(false)}
+          />
+        ) : (
+          <div className={styles.imgCard}>Imagem não disponivel</div>
+        )}
+        <h2 className={styles.hotelName}>{name}</h2>
+        <p>{desc}</p>
+        <div className={styles.hotelStars}> {renderStars()}</div>
+      </div>
+    </a>
+  );
 }
 
 HotelCard.propTypes = {
-    imgURL: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    desc: PropTypes.string.isRequired,
-    stars: PropTypes.number.isRequired,
-    hotelSiteURL: PropTypes.string.isRequired,
+  imgURL: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  stars: PropTypes.number.isRequired,
+  hotelSiteURL: PropTypes.string.isRequired,
 };
--- fdr-at1/src/pages/home/home.jsx
+++ fdr-at1/src/pages/home/home.jsx
@@ -1,14 +1,4 @@
-import HotelCard from '../../components/hotel_card/hotel_card';
 
 export default function Home() {
-    return (
-        <div>
-            <HotelCard
-                stars={3}
-                name='Bella Vista'
-                imgURL='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.viajeselcorteingles.es%2Fimagenes%2Fhoteles%2Fcuba%2Fvaradero%2Fh10001514%2F5.jpg&f=1&nofb=1&ipt=2ba22a1c7c7ce431f223cee3d12bce56c35a0c67a90254b702e608dcd779eabf&ipo=images'
-                desc='A 5-star hotel in the heart of Varadero.'
-            />
-        </div>
-    );
+  return <div></div>;
 }
