22530da5ed7a49326e81a731833d2dfea4cf6da4
ItzTas <talessabini2@gmail.com>
2024-09-23 15:44:56 -0400

    atv 3

--- fdr-at1/src/components/hotel_card/hotel_card.jsx
+++ fdr-at1/src/components/hotel_card/hotel_card.jsx
@@ -4,64 +4,64 @@ import styles from './hotel_card.module.css';
 import { Link } from 'react-router-dom';
 
 export default function HotelCard({
-    hotelURL,
-    imgURL,
-    name,
-    desc,
-    stars,
-    city,
-    state,
-    price,
+  hotelURL,
+  imgURL,
+  name,
+  desc,
+  stars,
+  city,
+  state,
+  price,
 }) {
-    const [imgLoaded, setImgLoaded] = useState(true);
-    if (desc.length > 60) {
-        desc = desc.substring(0, 57) + '...';
-    }
+  const [imgLoaded, setImgLoaded] = useState(true);
+  if (desc.length > 60) {
+    desc = desc.substring(0, 57) + '...';
+  }
 
-    function renderStars() {
-        const starElements = [];
-        for (let i = 0; i < stars; i++) {
-            starElements.push(
-                <span key={i} className={styles.hotelStar}>
-                    ⭐
-                </span>,
-            );
-        }
-        return starElements;
+  function renderStars() {
+    const starElements = [];
+    for (let i = 0; i < stars; i++) {
+      starElements.push(
+        <span key={i} className={styles.hotelStar}>
+          ⭐
+        </span>,
+      );
     }
-    return (
-        <Link className={styles.link} href={hotelURL}>
-            <div className={styles.hotelCard}>
-                {imgLoaded ? (
-                    <img
-                        className={styles.imgCard}
-                        src={imgURL}
-                        alt={name}
-                        onError={() => setImgLoaded(false)}
-                    />
-                ) : (
-                    <div className={styles.imgCard}>Imagem não disponivel</div>
-                )}
-                <h2 className={styles.hotelName}>{name}</h2>
-                <p>{desc}</p>
-                <div className={styles.hotelStars}> {renderStars()}</div>
-                <div className={styles.infos}>
-                    <div className={styles.state}>Estado: {state}</div>
-                    <div className={styles.city}>Cidade: {city}</div>
-                    <div className={styles.price}>Preço: R${price.toFixed(2)}</div>
-                </div>
-            </div>
-        </Link>
-    );
+    return starElements;
+  }
+  return (
+    <Link className={styles.link} to={hotelURL}>
+      <div className={styles.hotelCard}>
+        {imgLoaded ? (
+          <img
+            className={styles.imgCard}
+            src={imgURL}
+            alt={name}
+            onError={() => setImgLoaded(false)}
+          />
+        ) : (
+          <div className={styles.imgCard}>Imagem não disponivel</div>
+        )}
+        <h2 className={styles.hotelName}>{name}</h2>
+        <p>{desc}</p>
+        <div className={styles.hotelStars}> {renderStars()}</div>
+        <div className={styles.infos}>
+          <div className={styles.state}>Estado: {state}</div>
+          <div className={styles.city}>Cidade: {city}</div>
+          <div className={styles.price}>Preço: R${price.toFixed(2)}</div>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 HotelCard.propTypes = {
-    imgURL: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    desc: PropTypes.string.isRequired,
-    stars: PropTypes.number.isRequired,
-    hotelURL: PropTypes.string.isRequired,
-    city: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
+  imgURL: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  stars: PropTypes.number.isRequired,
+  hotelURL: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
--- fdr-at1/src/pages/home/home.jsx
+++ fdr-at1/src/pages/home/home.jsx
@@ -1,9 +1,45 @@
 import HotelList from '../../components/hotel_list/hotel_list';
 
 export default function Home() {
-    return (
-        <div>
-            <HotelList margin='20px' />
-        </div>
-    );
+  const initialHotels = [
+    {
+      name: 'Hotel A',
+      stars: 5,
+      desc: 'This is a hotel that will be used as the default for all customers',
+      city: 'São Paulo',
+      state: 'SP',
+      price: 500,
+      imgURL:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.voltaaomundo.pt%2Ffiles%2F2016%2F06%2Fconrad.jpg&f=1&nofb=1&ipt=ebfb3ffd57c9215019eef29393706a3bf3347822e5ffde7d6f5698813c62e873&ipo=images',
+      hotelURL: '/hotels/1',
+    },
+    {
+      name: 'Hotel B',
+      stars: 4,
+      desc: 'This is a hotel that will be used as the default for all customers',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      price: 400,
+      imgURL:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.sCyl_SbxWmK61J4Ab7gbcwHaEK%26pid%3DApi&f=1&ipt=5cfb11831c0a2909bb155dde5b64a131bcb497982a904b40d8ddc10337748f5d&ipo=images',
+      hotelURL: '/hotels/2',
+    },
+    {
+      name: 'Hotel C',
+      stars: 3,
+      desc: 'An affordable hotel with comfortable amenities.',
+      city: 'Curitiba',
+      state: 'PR',
+      price: 300,
+      imgURL:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.voltaaomundo.pt%2Ffiles%2F2016%2F06%2Fconrad.jpg&f=1&nofb=1&ipt=ebfb3ffd57c9215019eef29393706a3bf3347822e5ffde7d6f5698813c62e873&ipo=images',
+      hotelURL: '/hotels/3',
+    },
+  ];
+
+  return (
+    <div>
+      <HotelList hotels={initialHotels} margin='20px' />
+    </div>
+  );
 }
